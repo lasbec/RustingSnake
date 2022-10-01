65bdc719bba9fd64b88edefacf858e0d7790a965
Lasse Becker <lasse@cpsh.de>
2022-10-01 12:08:51 +0200

    checkpoint

---
+++ rustingSnake/src/snake.rs
@@ -0,0 +1,125 @@
+use piston_window::types::Color;
+use piston_window::{Context, G2d};
+use std::collections::LinkedList;
+
+use crate::draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+#[derive(Copy, Clone, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Block {
+    x: i32,
+    y: i32,
+}
+
+impl Block {
+    pub fn neighbour(&self, dir: Direction) -> Block {
+        match dir {
+            Direction::Up => Block {
+                x: self.x - 1,
+                y: self.y,
+            },
+            Direction::Down => Block {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::Left => Block {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Right => Block {
+                x: self.x,
+                y: self.y + 1,
+            },
+        }
+    }
+}
+
+pub struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>,
+}
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+        body.push_back(Block { x: x + 2, y });
+        body.push_back(Block { x: x + 1, y });
+        body.push_back(Block { x, y });
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        for block in &self.body {
+            draw_block(SNAKE_COLOR, block.x, block.y, con, g);
+        }
+    }
+
+    pub fn head_position(&self) -> &Block {
+        let head_block = self.body.front().unwrap();
+        head_block
+    }
+
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        match dir {
+            Some(d) => self.direction = d,
+            None => (),
+        }
+
+        let new_block = self.head_position().neighbour(self.direction);
+
+        self.body.push_front(new_block);
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
+
+    pub fn next_head(&self, dir: Option<Direction>) -> Block {
+        let moving_dir = match dir {
+            Some(d) => d,
+            None => self.direction,
+        };
+        self.head_position().neighbour(moving_dir)
+    }
+
+    pub fn restore_tail(&mut self) {
+        let blk = self.tail.clone().unwrap();
+        self.body.push_back(blk);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return false;
+    }
+}
